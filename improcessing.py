a18349403f217e4b7725e293671e9def9df904e9
Hex <35177464+Hexbugman213@users.noreply.github.com>
2020-11-23 22:22:57 -0600

    pausing here

--- improcessing.py
+++ improcessing.py
@@ -3,18 +3,36 @@ import random
 import string
 
 import wand
-from wand.image import Image
-from wand.display import display
-from wand.drawing import Drawing
-from wand.font import Font
-from wand.color import Color
-from wand.image import BaseImage
+from PIL import Image, ImageFont, ImageDraw
 
-futura = os.path.abspath("fonts/caption.otf").replace("\\", "/")
+futura = ImageFont.truetype("fonts/caption.otf")
 
 
 def get_random_string(length):
     return ''.join(random.choice(string.ascii_letters) for i in range(length))
+
+
+def wrap(font, text, line_width):
+    words = text.split()
+
+    lines = []
+    line = []
+
+    for word in words:
+        newline = ' '.join(line + [word])
+
+        w, h = font.getsize(newline)
+
+        if w > line_width:
+            lines.append(' '.join(line))
+            line = [word]
+        else:
+            line.append(word)
+
+    if line:
+        lines.append(' '.join(line))
+
+    return ('\n'.join(lines)).strip()
 
 
 async def imsave(image):
@@ -22,21 +40,19 @@ async def imsave(image):
     while True:
         name = f"temp/{get_random_string(8)}.{extension}"
         if not os.path.exists(name):
-            image.save(filename=name)
+            image.save(name)
             return name
 
 
 async def imcaption(image, cap):  # `image` is a file path and `cap` is a str
-    # image = "text-jpg"
-    with Image(filename=image) as img:
-        with Image(width=img.width, height=1000, background=Color("White")) as capimg:
-            capimg.options['pango:align'] = 'center'
-            capimg.options['pango:wrap'] = 'word-char'
-            capimg.options['pango:single-paragraph'] = 'false'
-            wand.font = futura
-            capimg.font_size = int(img.width / 13 * 1.333)
-            capimg.pseudo(capimg.width, capimg.height, pseudo=f"pango:{cap}")
-            capimg.gravity = "center"
-            capimg.extent(width=img.width, height=int(capimg.height + img.width / 13))
-            capimg.merge_layers('flatten')
-            return await imsave(capimg)
+
+    im = Image.open(image)
+    futuralocal = ImageFont.truetype("fonts/caption.otf", int(im.width / 13 * 1.333),
+                                     layout_engine=ImageFont.LAYOUT_RAQM)
+    cap = wrap(futuralocal, cap, im.width - ((im.width / 25) * 2))
+    capsize = futuralocal.getsize_multiline(cap, spacing=8)
+    imcap = Image.new("RGB", (im.width, capsize[1] + im.width), "#fff")
+    d = ImageDraw.Draw(imcap)
+    d.multiline_text(((im.width - capsize[0]) // 2, (imcap.height - capsize[1]) // 2), cap, (0, 0, 0), font=futuralocal,
+                     align='center', spacing=8)
+    return await imsave(imcap)
