8677f4fd009083ba10aae6bf64d46400601a3f93
Hex <35177464+Hexbugman213@users.noreply.github.com>
2020-11-23 23:08:45 -0600

    $esmcaption finished

--- improcessing.py
+++ improcessing.py
@@ -44,15 +44,23 @@ async def imsave(image):
             return name
 
 
+def get_concat_v(im1, im2):
+    dst = Image.new('RGBA', (im1.width, im1.height + im2.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (0, im1.height))
+    return dst
+
+
 async def imcaption(image, cap):  # `image` is a file path and `cap` is a str
 
     im = Image.open(image)
-    futuralocal = ImageFont.truetype("fonts/caption.otf", int(im.width / 13 * 1.333),
+    futuralocal = ImageFont.truetype("fonts/caption.otf", int(im.width / 13 * 1.3),
                                      layout_engine=ImageFont.LAYOUT_RAQM)
     cap = wrap(futuralocal, cap, im.width - ((im.width / 25) * 2))
-    capsize = futuralocal.getsize_multiline(cap, spacing=8)
-    imcap = Image.new("RGB", (im.width, capsize[1] + im.width), "#fff")
+    capsize = futuralocal.getsize_multiline(cap, spacing=(futuralocal.size // 4))
+    imcap = Image.new("RGB", (im.width, capsize[1] + (im.width // 8)), "#fff")
     d = ImageDraw.Draw(imcap)
-    d.multiline_text(((im.width - capsize[0]) // 2, (imcap.height - capsize[1]) // 2), cap, (0, 0, 0), font=futuralocal,
-                     align='center', spacing=8)
-    return await imsave(imcap)
+    d.multiline_text(((im.width - capsize[0]) // 2, (imcap.height - capsize[1]) // 2 - (futuralocal.size // 8)), cap,
+                     (0, 0, 0), font=futuralocal,
+                     align='center', spacing=(futuralocal.size // 4))
+    return await imsave(get_concat_v(imcap, im))
