179606f029ce2e12ace7de80605ca1ecc734bf0f
Hex <35177464+Hexbugman213@users.noreply.github.com>
2020-11-23 21:14:50 -0600

    giving up on imagemagick

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import logging
+import os
+import random
+import string
+import sys
+import traceback
 import discord
 from discord.ext import commands
-import asyncio
-import logging
-import datetime
-import re
+import improcessing
+
 
 logging.basicConfig(format='%(levelname)s:[%(asctime)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
                     level=logging.INFO)
@@ -13,14 +17,33 @@ bot = commands.Bot(command_prefix='$', description='captionbot')
 bot.remove_command('help')
 
 
+def get_random_string(length):
+    return ''.join(random.choice(string.ascii_letters) for i in range(length))
+
+
+async def saveattachment(attachment: discord.Attachment):
+    extension = attachment.filename.split(".")[-1]
+    while True:
+        name = f"temp/{get_random_string(8)}.{extension}"
+        if not os.path.exists(name):
+            await attachment.save(name)
+            return name
+
 @bot.event
 async def on_ready():
-    global awatingmonday
-    # await database.firsttime()
     logging.info(f"Logged in as {bot.user.name}!")
     game = discord.Activity(name=f"with your files",
                             type=discord.ActivityType.watching)
 
+
+@bot.command()
+async def caption(ctx, *, cap):
+    if len(ctx.message.attachments):
+        file = await saveattachment(ctx.message.attachments[0])
+        result = await improcessing.imcaption(file, cap)
+        await ctx.send(file=discord.File(result))
+    else:
+        await ctx.send("❌ Message has no attachment.")
 
 @bot.command()
 @commands.is_owner()
@@ -34,6 +57,31 @@ async def say(ctx, *, msg):
 async def shutdown(ctx):
     await ctx.send("✅ Shutting Down...")
     await bot.close()
+
+
+@bot.listen()
+async def on_command(ctx):
+    logging.info(f"@{ctx.message.author.name}#{ctx.message.author.discriminator} ({ctx.message.author.display_name}) "
+                 f"ran '{ctx.message.content}' in channel #{ctx.channel.name} in server {ctx.guild}")
+
+
+@bot.listen()
+async def on_command_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.CommandNotFound):
+        err = f"⁉ Command `{ctx.message.content}` does not exist."
+        logging.warning(err)
+        await ctx.send(err)
+    elif isinstance(error, discord.ext.commands.errors.NotOwner):
+        err = "❌ You are not authorized to use this command."
+        logging.warning(err)
+        await ctx.send(err)
+    elif isinstance(error, discord.ext.commands.errors.CommandOnCooldown):
+        err = "⏱ " + str(error).replace("@", "\\@")
+        logging.warning(err)
+        await ctx.send(err)
+    else:
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+        await ctx.send("‼ `" + str(error).replace("@", "\\@") + "`")
 
 
 with open('token.txt') as f:  # not on github for obvious reasons
