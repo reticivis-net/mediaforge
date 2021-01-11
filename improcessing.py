aef7f013e415b06ed95a389fb7cb3e6d1dd76de0
Hex <35177464+Hexbugman213@users.noreply.github.com>
2021-01-10 23:09:37 -0600

    video support and multiprocessing for BIG SPEED

--- improcessing.py
+++ improcessing.py
@@ -1,3 +1,4 @@
+import asyncio
 import glob
 import logging
 import os
@@ -9,6 +10,20 @@ import imgkit
 from PIL import Image
 import re
 from winmagic import magic
+from multiprocessing import Pool
+import functools
+
+
+def disable_logging(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        logging.disable(logging.INFO)
+        result = func(*args, **kwargs)
+        logging.disable(logging.NOTSET)
+        return result
+
+    return wrapper
+
 
 options = {
     "enable-local-file-access": None,
@@ -39,6 +54,44 @@ def replaceall(text, rep):
     return text
 
 
+async def run_command(args):
+    """Run command in subprocess.
+    Example from:
+        http://asyncio.readthedocs.io/en/latest/subprocess.html
+    """
+    # Create subprocess
+    process = await asyncio.create_subprocess_shell(
+        args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+
+    # Status
+    print("Started: %s, pid=%s" % (args, process.pid), flush=True)
+
+    # Wait for the subprocess to finish
+    stdout, stderr = await process.communicate()
+
+    # Progress
+    if process.returncode == 0:
+        print(
+            "Done: %s, pid=%s, result: %s"
+            % (args, process.pid, stdout.decode().strip()),
+            flush=True,
+        )
+    else:
+        print(
+            "Failed: %s, pid=%s, result: %s"
+            % (args, process.pid, stderr.decode().strip()),
+            flush=True,
+        )
+
+    # Result
+    result = stdout.decode().strip()
+
+    # Return stdout
+    return result
+
+
+@disable_logging
 def imgkitstring(torender, tosavename=None):
     if tosavename is None:
         extension = "png"
@@ -69,48 +122,26 @@ def get_frame_rate(filename):
     return -1
 
 
-def ffmpegsplit(image):
+async def ffmpegsplit(image):
     logging.info("[improcessing] Splitting frames...")
-    subprocess.call(f"ffmpeg -i {image} -vsync 0 {image.replace('.', '')}%09d.png", stdout=subprocess.DEVNULL,
-                    stderr=subprocess.DEVNULL)
-    files = glob.glob(f"{image.replace('.', '')}*.png")
-    return files, f"{image.replace('.', '')}%09d.png"
+    await run_command(f"ffmpeg -i {image} -vsync 1 {image.split('.')[0]}%09d.png")
+    files = glob.glob(f"{image.split('.')[0]}*.png")
+    return files, f"{image.split('.')[0]}%09d.png"
 
 
-async def handleanimated(image, caption, capfunction):
-    try:
-        with Image.open(image) as im:
-            anim = getattr(im, "is_animated", False)
-    except Exception:  # invalid image or video?
-        logging.warning("Failed to open as image... handling unimplemented.")
-        # https://stackoverflow.com/a/56526114/9044183
-        mime = magic.Magic(mime=True)
-        filename = mime.from_file(image)
-        if filename.find('video') != -1:
-            print('it is video')
-    if anim:
-        logging.info("[improcessing] Gif or similair detected...")
-        frames, name = ffmpegsplit(image)
-        fps = get_frame_rate(image)
-        logging.info("[improcessing] Processing frames...")
-        for i, f in enumerate(frames):
-            await capfunction(f, caption, f.replace('.png', 'rendered.png'))
-            os.remove(f)
-        logging.info("[improcessing] Joining frames...")
-        extension = "gif"
-        while True:
-            outname = f"temp/{get_random_string(8)}.{extension}"
-            if not os.path.exists(outname):
-                break
-        subprocess.call(f"gifski -o {outname} --fps {fps} {name.replace('.png', 'rendered.png').replace('%09d', '*')}",
-                        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        return outname
-    else:
-        await capfunction(image, caption)
+async def splitaudio(video):
+    logging.info("[improcessing] Splitting audio...")
+    extension = "aac"
+    while True:
+        name = f"temp/{get_random_string(8)}.{extension}"
+        if not os.path.exists(name):
+            break
+    await run_command(f"ffmpeg -i {video} -vn -acodec copy {name}")
+    return name
 
 
-async def imcaption(image, caption, tosavename=None):
-    logging.info("[improcessing] Rendering image...")
+def imcaption(image, caption, tosavename=None):
+    logging.info(f"[improcessing] Rendering {image}...")
     with Image.open(image) as im:
         imagewidth = im.size[0]  # seems ineffecient but apparently fast?
     replacedict = {
@@ -123,3 +154,73 @@ async def imcaption(image, caption, tosavename=None):
     torender = replaceall(filetostring("caption.html"), replacedict)
     rendered = imgkitstring(torender, tosavename)
     return rendered
+
+
+async def handleanimated(image, caption, capfunction):
+    try:
+        with Image.open(image) as im:
+            anim = getattr(im, "is_animated", False)
+    except IOError:  # either invalid file or a valid video
+        # https://stackoverflow.com/a/56526114/9044183
+        mime = magic.Magic(mime=True)
+        filename = mime.from_file(image)
+        if filename.find('video') != -1:
+            logging.warning("[improcessing] Video detected.")
+            logging.info("[improcessing] Splitting frames...")
+            frames, name = await ffmpegsplit(image)
+            audio = await splitaudio(image)
+            fps = get_frame_rate(image)
+            logging.info("[improcessing] Processing frames...")
+            capargs = []
+            for i, frame in enumerate(frames):
+                capargs.append((frame, caption, frame.replace('.png', '_rendered.png')))
+            pool = Pool(16)
+            pool.starmap_async(imcaption, capargs)
+            pool.close()
+            pool.join()
+            logging.info("[improcessing] Joining frames...")
+            extension = "mp4"
+            while True:
+                outname = f"temp/{get_random_string(8)}.{extension}"
+                if not os.path.exists(outname):
+                    break
+            await run_command(f"ffmpeg -r {fps} -start_number 1 -i {name.replace('.png', '_rendered.png')} "
+                              f"-i {audio} -c:a aac -shortest "
+                              f"-c:v libx264 -crf 25 -pix_fmt yuv420p "
+                              f"-vf \"crop=trunc(iw/2)*2:trunc(ih/2)*2\" {outname}")
+            # cleanup
+            logging.info("[improcessing] Cleaning files...")
+            for f in glob.glob(name.replace('%09d', '*')):
+                os.remove(f)
+            os.remove(audio)
+            return outname
+        else:
+            raise Exception("File given is not valid image or video.")
+    else:
+        if anim:  # gif
+            logging.info("[improcessing] Gif or similair detected.")
+            logging.info("[improcessing] Splitting frames...")
+            frames, name = await ffmpegsplit(image)
+            fps = get_frame_rate(image)
+            logging.info("[improcessing] Processing frames...")
+            capargs = []
+            for i, frame in enumerate(frames):
+                capargs.append((frame, caption, frame.replace('.png', '_rendered.png')))
+            pool = Pool(32)
+            pool.starmap(imcaption, capargs)
+            pool.close()
+            pool.join()
+            logging.info("[improcessing] Joining frames...")
+            extension = "gif"
+            while True:
+                outname = f"temp/{get_random_string(8)}.{extension}"
+                if not os.path.exists(outname):
+                    break
+            await run_command(
+                f"gifski -o {outname} --fps {fps} {name.replace('.png', '_rendered.png').replace('%09d', '*')}")
+            logging.info("[improcessing] Cleaning files...")
+            for f in glob.glob(name.replace('%09d', '*')):
+                os.remove(f)
+            return outname
+        else:  # normal image
+            await capfunction(image, caption)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import glob
 import logging
 import os
 import random
@@ -9,112 +10,118 @@ from discord.ext import commands
 import improcessing
 import aiohttp
 import aiofiles
-import imgkit
 
 logging.basicConfig(format='%(levelname)s:[%(asctime)s] %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
                     level=logging.INFO)
-logging.info(f"Discord Version {discord.__version__}")
-logging.info("Initalizing")
-bot = commands.Bot(command_prefix='$', description='captionbot')
-bot.remove_command('help')
+if __name__ == '__main__':
+    logging.info(f"Discord Version {discord.__version__}")
+    logging.info("Initalizing")
+    bot = commands.Bot(command_prefix='$', description='captionbot')
+    bot.remove_command('help')
+    for f in glob.glob('temp/*'):
+        os.remove(f)
 
 
-def get_random_string(length):
-    return ''.join(random.choice(string.ascii_letters) for i in range(length))
+    def get_random_string(length):
+        return ''.join(random.choice(string.ascii_letters) for i in range(length))
 
 
-async def saveurl(url):
-    logging.info(f"Saving url {url}")
-    extension = url.split(".")[-1].split("?")[0]
-    while True:
-        name = f"temp/{get_random_string(8)}.{extension}"
-        if not os.path.exists(name):
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url) as resp:
-                    if resp.status == 200:
-                        f = await aiofiles.open(name, mode='wb')
-                        await f.write(await resp.read())
-                        await f.close()
-                    else:
-                        logging.error(f"aiohttp status {resp.status}")
-                        logging.error(f"aiohttp status {await resp.read()}")
+    async def saveurl(url):
+        logging.info(f"Saving url {url}")
+        extension = url.split(".")[-1].split("?")[0]
+        while True:
+            name = f"temp/{get_random_string(8)}.{extension}"
+            if not os.path.exists(name):
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url) as resp:
+                        if resp.status == 200:
+                            f = await aiofiles.open(name, mode='wb')
+                            await f.write(await resp.read())
+                            await f.close()
+                        else:
+                            logging.error(f"aiohttp status {resp.status}")
+                            logging.error(f"aiohttp status {await resp.read()}")
 
-            return name
-
-
-async def imagesearch(ctx):
-    async for m in ctx.channel.history(limit=50):
-        if len(m.embeds):
-            if m.embeds[0].type == "image" or m.embeds[0].type == "video":
-                return await saveurl(m.embeds[0].url)
-        elif len(m.attachments):
-            if m.attachments[0].width:
-                return await saveurl(m.attachments[0].url)
-    return False
+                return name
 
 
-@bot.event
-async def on_ready():
-    logging.info(f"Logged in as {bot.user.name}!")
-    game = discord.Activity(name=f"with your files",
-                            type=discord.ActivityType.watching)
+    async def imagesearch(ctx):
+        async for m in ctx.channel.history(limit=50):
+            if len(m.embeds):
+                if m.embeds[0].type == "image" or m.embeds[0].type == "video":
+                    return await saveurl(m.embeds[0].url)
+            elif len(m.attachments):
+                if m.attachments[0].width:
+                    return await saveurl(m.attachments[0].url)
+        return False
 
 
-@bot.command()
-async def esmcaption(ctx, *, cap):
-    await ctx.channel.trigger_typing()
-    logging.info("Getting image...")
-    file = await imagesearch(ctx)
-    if file:
-        logging.info("Processing image...")
-        result = await improcessing.handleanimated(file, cap, improcessing.imcaption)
-        logging.info("Uploading image...")
-        await ctx.send(file=discord.File(result))
-        os.remove(file)
-        os.remove(result)
-    else:
-        await ctx.send("❌ No file found.")
+    @bot.event
+    async def on_ready():
+        logging.info(f"Logged in as {bot.user.name}!")
+        game = discord.Activity(name=f"with your files",
+                                type=discord.ActivityType.watching)
 
 
-@bot.command()
-@commands.is_owner()
-async def say(ctx, *, msg):
-    await ctx.message.delete()
-    await ctx.channel.send(msg)
+    @bot.command()
+    async def esmcaption(ctx, *, cap):
+
+        logging.info("Getting image...")
+        file = await imagesearch(ctx)
+        if file:
+            logging.info("Processing image...")
+            msg = await ctx.send("Processing...")
+            result = await improcessing.handleanimated(file, cap, improcessing.imcaption)
+            await ctx.channel.trigger_typing()
+            logging.info("Uploading image...")
+            await ctx.send(file=discord.File(result))
+            await msg.delete()
+            os.remove(file)
+            os.remove(result)
+        else:
+            await ctx.send("❌ No file found.")
 
 
-@bot.command()
-@commands.is_owner()
-async def shutdown(ctx):
-    await ctx.send("✅ Shutting Down...")
-    await bot.close()
+    @bot.command()
+    @commands.is_owner()
+    async def say(ctx, *, msg):
+        await ctx.message.delete()
+        await ctx.channel.send(msg)
 
 
-@bot.listen()
-async def on_command(ctx):
-    logging.info(f"@{ctx.message.author.name}#{ctx.message.author.discriminator} ({ctx.message.author.display_name}) "
-                 f"ran '{ctx.message.content}' in channel #{ctx.channel.name} in server {ctx.guild}")
+    @bot.command()
+    @commands.is_owner()
+    async def shutdown(ctx):
+        await ctx.send("✅ Shutting Down...")
+        await bot.close()
 
 
-@bot.listen()
-async def on_command_error(ctx, error):
-    if isinstance(error, discord.ext.commands.errors.CommandNotFound):
-        err = f"⁉ Command `{ctx.message.content}` does not exist."
-        logging.warning(err)
-        await ctx.send(err)
-    elif isinstance(error, discord.ext.commands.errors.NotOwner):
-        err = "❌ You are not authorized to use this command."
-        logging.warning(err)
-        await ctx.send(err)
-    elif isinstance(error, discord.ext.commands.errors.CommandOnCooldown):
-        err = "⏱ " + str(error).replace("@", "\\@")
-        logging.warning(err)
-        await ctx.send(err)
-    else:
-        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-        await ctx.send("‼ `" + str(error).replace("@", "\\@") + "`")
+    @bot.listen()
+    async def on_command(ctx):
+        logging.info(
+            f"@{ctx.message.author.name}#{ctx.message.author.discriminator} ({ctx.message.author.display_name}) "
+            f"ran '{ctx.message.content}' in channel #{ctx.channel.name} in server {ctx.guild}")
 
 
-with open('token.txt') as f:  # not on github for obvious reasons
-    token = f.read()
-bot.run(token)
+    @bot.listen()
+    async def on_command_error(ctx, error):
+        if isinstance(error, discord.ext.commands.errors.CommandNotFound):
+            err = f"⁉ Command `{ctx.message.content}` does not exist."
+            logging.warning(err)
+            await ctx.send(err)
+        elif isinstance(error, discord.ext.commands.errors.NotOwner):
+            err = "❌ You are not authorized to use this command."
+            logging.warning(err)
+            await ctx.send(err)
+        elif isinstance(error, discord.ext.commands.errors.CommandOnCooldown):
+            err = "⏱ " + str(error).replace("@", "\\@")
+            logging.warning(err)
+            await ctx.send(err)
+        else:
+            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+            await ctx.send("‼ `" + str(error).replace("@", "\\@") + "`")
+
+
+    with open('token.txt') as f:  # not on github for obvious reasons
+        token = f.read()
+    bot.run(token)
