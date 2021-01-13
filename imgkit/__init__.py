0b446957044b7ae88e8689eddc7cbcdd87f33602
Melody <reticivis@gmail.com>
2021-01-13 10:40:53 -0600

    FIXED THE STGUPID FUCKING SPAM BUG FINALLY OMFG https://github.com/jarrekk/imgkit/issues/52

---
+++ imgkit/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Wkhtmltopdf python wrapper to convert html to image using the webkit rendering engine and qt
+"""
+
+__author__ = 'jarrekk'
+__contact__ = 'me@jarrekk.com'
+__version__ = '1.0.2'
+__homepage__ = 'https://github.com/jarrekk/imgkit'
+__license__ = 'MIT'
+
+from .imgkit import IMGKit
+from .api import from_url, from_file, from_string, config
