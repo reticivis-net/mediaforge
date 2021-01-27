77f993cf3f985e33f7fabc79bb75367cee53d43e
Melody <reticivis@gmail.com>
2021-01-27 11:41:54 -0600

    finished maybe? gonna do some user testing lol

--- config.example.py
+++ config.example.py
@@ -1,4 +1,4 @@
-# COPY THIS FILE INTO A FILE CALLED config.py AND THEN CHANGE THE VALUES AS NEEDED.
+# COPY THIS FILE INTO A FILE CALLED config.py AND CHANGE THE VALUES AS NEEDED.
 # discord API bot token https://discord.com/developers/applications
 bot_token = "EXAMPLE_TOKEN"
 # tenor API key https://tenor.com/developer/keyregistration
@@ -25,6 +25,13 @@ emojis = {
     "2exclamation": "<:eemark:803791399710883871>",
     "working": "<a:working:803801825605320754>",
     "clock": "<:clockmark:803803703169515541>"
+}
+# up to 25 tips that can show when using $help tips
+tips = {
+    "Media Searching": "MediaForge automatically searches for any media in a channel. Reply to a message with the command to search that message first.",
+    "GIFs and MP4s": "MediaForge works faster with MP4 files, due to GIF conversion not being fast.",
+    "Tenor GIFs": "All tenor GIFs are interpreted as MP4s by default due to their superior quality. Use `tenorgif` to send the raw GIF url or `togif` to convert any video to a gif.",
+    "Self-Hosting": "MediaForge is completely open source and anyone can host a clone themself!\nhttps://github.com/HexCodeFFF/captionbot"
 }
 # prefix for commands
 command_prefix = "$"
