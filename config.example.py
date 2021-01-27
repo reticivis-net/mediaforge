5fcbab950ca8ad9c3dd9f1109e3f4aeb690d85b1
Hex <35177464+Hexbugman213@users.noreply.github.com>
2021-01-26 20:06:08 -0600

    better emojis

--- config.example.py
+++ config.example.py
@@ -10,11 +10,21 @@ chrome_driver_linux = "chromedriver87"
 # the number of instances of chromedriver to run for caption processing.
 # more means faster processing of videos and better under heavy load but also uses more PC resources!
 chrome_driver_instances = 20
+# NOTICE is recommended, INFO prints more information about what bot is doing, WARNING only prints errors.
+log_level = "NOTICE"
 # maximum number of frames for an input file.
 max_frames = 1024
 # amount of seconds cooldown per user commands have. set to 0 to disable cooldown
 cooldown = 1
-# NOTICE is recommended, INFO prints more information about what bot is doing, WARNING only prints errors.
-log_level = "NOTICE"
+# the text to use for different messages, can be custom emojis or just unicode
+emojis = {
+    "x": "<:xmark:803792052932444180>",
+    "warning": "<:wmark:803791399782580284>",
+    "question": "<:qmark:803791399615070249>",
+    "exclamation_question": "<:eqmark:803791399501168641>",
+    "2exclamation": "<:eemark:803791399710883871>",
+    "working": "<a:working:803801825605320754>",
+    "clock": "<:clockmark:803803703169515541>"
+}
 # prefix for commands
 command_prefix = "$"
