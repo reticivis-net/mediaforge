97f39a4ae5dd58e1d36b25b442dd490efc5da2c7
Hex <35177464+HexCodeFFF@users.noreply.github.com>
2021-06-02 20:10:54 -0500

    motherfucker

--- improcessing.py
+++ improcessing.py
@@ -692,7 +692,7 @@ async def addaudio(files):
             outname = temp_file("mp3")
         else:
             outname = temp_file("mp4")
-        await run_command("ffmpeg", "-i", media, "-i", audio, "-filter_complex",
+        await run_command("ffmpeg", "-i", media, "-i", audio, "-max_muxing_queue_size", "4096", "-filter_complex",
                           "[0:a][1:a]amix=inputs=2:dropout_transition=100000:duration=longest[a];[a]volume=2[a]",
                           "-map", "0:v?", "-map", "[a]", "-c:a", "lbmp3lame" if mt == "AUDIO" else "aac", outname)
         return outname
