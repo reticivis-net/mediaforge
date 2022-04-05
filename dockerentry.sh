d24b7ac4244674de9d624b07f082275e58c17586
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-04-04 21:42:38 -0500

    hmm

--- dockerentry.sh
+++ dockerentry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PS3='What would you like to do? '
-foods=("Run MediaForge" "Edit Config" "Update MediaForge" "Quit")
+foods=("Run MediaForge" "Edit Config" "Update MediaForge" "Debug Shell" "Quit")
 select fav in "${foods[@]}"; do
   case $fav in
   "Run MediaForge")
@@ -10,7 +10,15 @@ select fav in "${foods[@]}"; do
     nano config.py
     ;;
   "Update MediaForge")
-    git pull
+    # remote isnt set up by default when container is set up
+    if [ ! -d ".git" ]; then
+      git init .
+      git remote add origin https://github.com/HexCodeFFF/mediaforge.git
+    fi
+    git pull origin master
+    ;;
+  "Debug Shell")
+    /bin/bash
     ;;
   "Quit")
     exit
