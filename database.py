968a007844bb7f94f9f9b4056b933ddd271d5c62
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-04-16 23:22:37 -0500

    refactor part 1

---
+++ database.py
@@ -0,0 +1,19 @@
+import asyncio
+
+import aiosqlite
+from nextcord.ext import commands
+
+import config
+
+db: aiosqlite.Connection
+
+
+async def create_db():
+    global db
+    db = await aiosqlite.connect(config.db_filename)
+
+
+class CreateDB(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        asyncio.get_event_loop().run_until_complete(create_db())
