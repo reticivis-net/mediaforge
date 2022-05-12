cf7abb5c8f045278a05c54ecac848104211dc016
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-05-12 15:28:36 -0500

    forgo(r|t) to commit this

--- v2render.py
+++ v2render.py
@@ -1,8 +1,19 @@
-from celery import Celery
+import typing
+
+from billiard.einfo import ExceptionInfo
+from celery import Celery, Task
 
 app = Celery('tasks', backend='rpc://guest@localhost//', broker='pyamqp://guest@localhost//')
 
 
-@app.task
+class CallbackTask(Task):
+    def on_success(self, retval: typing.Any, task_id: str, args: tuple, kwargs: dict):
+        pass
+
+    def on_failure(self, exc: Exception, task_id: str, args: tuple, kwargs: dict, einfo: ExceptionInfo):
+        pass
+
+
+@app.task(base=CallbackTask)  # this does the trick
 def add(x, y):
     return x + y
