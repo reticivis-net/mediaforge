c9547c60363b73c26ad726cc1e42d5506b412c49
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-09-02 18:33:55 -0500

    this is nice

---
+++ v2queue.py
@@ -0,0 +1,24 @@
+import asyncio
+import os
+import typing
+
+import config
+
+sem = asyncio.Semaphore(config.workers or os.cpu_count() or 1)
+queued = 0
+
+
+async def enqueue(task: typing.Coroutine):
+    global queued
+    queued += 1
+    # only allows a certian amount of tasks inside the context at once
+    # quick tests show that its roughly FIFO but there are libraries if needed
+    async with sem:
+        try:
+            res = await task
+        except Exception as e:
+            queued -= 1
+            raise e
+        else:
+            queued -= 1
+            return res
